fda9d26dd3f93af2801e03d3b74bf1f468289228
eapo <e@eaposztrof.com>
2022-09-25 10:45:52 +0300

    Add WarningIcon

--- ui/tasks/TasksHeader.jsx
+++ ui/tasks/TasksHeader.jsx
@@ -1,12 +1,12 @@
 import { Box, Heading, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
-import { CheckCircleIcon, CircleIcon } from '@chakra-ui/icons';
+import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons';
 
 export const TasksHeader = () => (
   <Stack as={Box} textAlign="center" spacing={{ base: 8 }} py={{ base: 10 }}>
     <Heading fontWeight={600}>
       <Text as="span" bgGradient="linear(to-l, #675AAA, #4399E1)" bgClip="text">
-        List of <CheckCircleIcon /> Goods & Services and <CircleIcon /> Needs
+        List of <CheckCircleIcon /> Goods & Services and <WarningIcon /> Needs
       </Text>
     </Heading>
   </Stack>
